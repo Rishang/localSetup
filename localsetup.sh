1a2203e9b68b880e2a165548439c927de00c9088
Rishang <rishangbhavsarcs@gmail.com>
2021-10-13 00:44:07 +0530

    opts

--- localsetup.sh
+++ localsetup.sh
@@ -37,7 +37,7 @@ function _print_help {
     echo "-u       | Update localsetup"
 }
 
-while getopts ":i:u:h" opt; do
+while getopts ":iuh" opt; do
     case "${opt}" in
         i)  
             i=${OPTARG}
